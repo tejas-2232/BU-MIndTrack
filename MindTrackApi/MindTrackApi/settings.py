80d86f187f613cfc0da577512486880665a56291
Tejas Bachhav <tejasbachhav98@gmail.com>
2024-02-18 01:56:14 +0000

    commented postgresql and added sqlite3

--- MindTrackApi/MindTrackApi/settings.py
+++ MindTrackApi/MindTrackApi/settings.py
@@ -104,18 +104,19 @@ WSGI_APPLICATION = "MindTrackApi.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "MindTrack",
-        "USER": "postgres",
-        "PASSWORD":"PostgreSQLService@24/7", 
-        "HOST":"localhost",
-        "PORT":"5432",
-    }
-    #{
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
+    "default": 
+    # {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": "MindTrack",
+    #     "USER": "postgres",
+    #     "PASSWORD":"PostgreSQLService@24/7", 
+    #     "HOST":"localhost",
+    #     "PORT":"5432",
     # }
+    {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
