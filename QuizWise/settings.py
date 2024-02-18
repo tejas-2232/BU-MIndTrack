9f23b7da7ea89271443f60ee0d301fb8bf8c56e6
tejas-2232 <tejasbachhav98@gmail.com>
2024-02-18 11:27:59 -0500

    'added pg creds for connection to rds

--- QuizWise/settings.py
+++ QuizWise/settings.py
@@ -84,19 +84,19 @@ WSGI_APPLICATION = 'QuizWise.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': 
-    # {
-    #     "ENGINE": "django.db.backends.postgresql",
-    #     "NAME": "QuizWise",
-    #     "USER": "postgres",
-    #     "PASSWORD":"postgres123",
-    #     "HOST":"database-1.cy1msonklglr.us-east-1.rds.amazonaws.com",
-    #     "PORT":"5432",
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': 
+    {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "QuizWise",
+        "USER": "postgres",
+        "PASSWORD":"postgres123",
+        "HOST":"database-1.cy1msonklglr.us-east-1.rds.amazonaws.com",
+        "PORT":"5432",
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
